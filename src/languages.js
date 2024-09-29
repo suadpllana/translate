b9180d20c8f7631f195c55ef1155caa44cba24fd
suadpllana <suadpllana14@gmail.com>
2024-09-30 01:32:52 +0200

    first commit

--- src/languages.js
+++ src/languages.js
@@ -1,61 +1,41 @@
 export const languageData = [
     { region: "sq", language: "Albanian" },
     { region: "af", language: "Afrikaans" },
-
     { region: "ar", language: "Arabic" },
-
     { region: "az", language: "Azerbaijani" },
     { region: "hy", language: "Armenian" },
     { region: "bg", language: "Bulgarian" },
-
     { region: "bs", language: "Bosnian" },
-    { region: "ca", language: "Catalan" },
     { region: "cs", language: "Czech" },
     { region: "hr", language: "Croatian" },
-
-
     { region: "da", language: "Danish" },
     { region: "nl", language: "Dutch" },
-    { region: "prs", language: "Dari" },
+
     { region: "en", language: "English" },
     { region: "et", language: "Estonian" },
     { region: "fi", language: "Finnish" },
-    { region: "fj", language: "Fijian" },
-    { region: "fr", language: "French" },
 
+    { region: "fr", language: "French" },
     { region: "fil", language: "Filipino" },
     { region: "de", language: "German" },
     { region: "el", language: "Greek" },
-    { region: "gu", language: "Gujarati" },
+
     { region: "he", language: "Hebrew" },
     { region: "hi", language: "Hindi" },
-
-
     { region: "hu", language: "Hungarian" },
     { region: "ga", language: "Irish" },
     { region: "id", language: "Indonesian" },
     { region: "is", language: "Icelandic" },
     { region: "it", language: "Italian" },
-
     { region: "ja", language: "Japanese" },
-    { region: "kk", language: "Kazakh" },
 
-    { region: "kn", language: "Kannada" },
+
     { region: "ko", language: "Korean" },
-
-
     { region: "lt", language: "Lithuanian" },
     { region: "lv", language: "Latvian" },
 
-    { region: "my", language: "Myanmar (Burmese)" },
-    { region: "nb", language: "Norwegian" },
-    { region: "ne", language: "Nepali" },
-    { region: "or", language: "Odia" },
-    { region: "fa", language: "Persian" },
-    { region: "pa", language: "Punjabi" },
     { region: "pl", language: "Polish" },
     { region: "pt", language: "Portuguese (Brazil)" },
-    { region: "pt-PT", language: "Portuguese (Portugal)" },
 
     { region: "ro", language: "Romanian" },
     { region: "ru", language: "Russian" },
@@ -63,22 +43,14 @@ export const languageData = [
     { region: "sl", language: "Slovenian" },
     { region: "sm", language: "Samoan" },
     { region: "es", language: "Spanish" },
-    { region: "sr-Cyrl", language: "Serbian (Cyrillic)" },
-    { region: "sr-Latn", language: "Serbian (Latin)" },
+
     { region: "sv", language: "Swedish" },
-    { region: "sw", language: "Swahili" },
-    { region: "ta", language: "Tamil" },
-    { region: "te", language: "Telugu" },
-    { region: "th", language: "Thai" },
 
-
-    { region: "to", language: "Tongan" },
+,
     { region: "tr", language: "Turkish" },
     { region: "ty", language: "Tahitian" },
     { region: "uk", language: "Ukrainian" },
-    { region: "ur", language: "Urdu" },
+
     { region: "vi", language: "Vietnamese" },
     { region: "cy", language: "Welsh" },
-
-
 ];
